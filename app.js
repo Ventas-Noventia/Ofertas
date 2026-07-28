243397a69d7e5c8fb28c43f80aa8d2f9ab1526c0
edwinmovexa-star <edwinmovexa@gmail.com>
2026-07-28 13:09:04 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1858,7 +1858,7 @@ function imprimirEtiqueta(s) {
   printArea.id = "printArea";
   printArea.innerHTML = `
     <div class="print-header">
-    <p><img src="/img/logo.JPG" alt="Noventia" style="width: 200px"/></p>
+    <p><img src="logo.JPG" alt="Noventia" style="width: 200px"/></p>
       <strong class="print-type">${escapeHtml(tipoTexto)}</strong>
       <h1>${escapeHtml(s.folio || "SIN FOLIO")}</h1>
     </div>
